9ade8a8d4fda7f31dcc7f95cfbf936a668f52b9e
Paulo Moore <pimoore@uc.cl>
2021-02-25 13:50:40 -0300

    Navbar Desktop Version ready + dropdown cclock functionality for mobile version

---
+++ script.js
@@ -0,0 +1,21 @@
+function dropdownMenu() {
+	document.getElementById('main-menu').classList.toggle('show');
+}
+
+function dropdownBootcamp() {
+	document.getElementById('bootcamp').classList.toggle('show-2');
+}
+
+function dropdownPD() {
+	document
+		.getElementById('professional-development')
+		.classList.toggle('show-2');
+}
+
+function dropdownOutcomes() {
+	document.getElementById('outcomes').classList.toggle('show-2');
+}
+
+function dropdownEvents() {
+	document.getElementById('events').classList.toggle('show-2');
+}
